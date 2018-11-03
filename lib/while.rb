e82d23b06dcf143081ac7965ecdd1abd31f458c7
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-03 15:30:24 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -5,9 +5,9 @@ while levitation_force < 10
   puts "Wingardium Leviosa"
   levitation_force += 1
 end
-end
 
 until levitation_force == 10
   puts "Wingardium Leviosa"
   levitation_force += 1
+end
 end
